651dafd62b2d741657ef1defc59959c955a43d88
stscoundrel <silvennoinen.sampo@gmail.com>
2020-05-11 16:40:27 +0300

    Transient: add “multiple” versions of get/set/delete

--- src/Cache/Transient.php
+++ src/Cache/Transient.php
@@ -142,47 +142,63 @@ class Transient implements PSR16\CacheInterface
     abstract public function clear();
 
     /**
-     * Obtains multiple cache items by their unique keys.
+     * Get cached results for array of values.
      *
-     * @param iterable $keys    A list of keys that can be obtained in a single operation.
-     * @param mixed    $default Default value to return for keys that do not exist.
-     *
-     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
+     * @param Iterable $keys to fetch.
+     * @param mixed $default value for missed ones.
      */
-    abstract public function getMultiple($keys, $default = null);
+    public function getMultiple($keys, $default = null)
+    {
+        $results = array();
+
+        foreach($keys as $key) :
+            $results[ $key ] = $this->get( $key, $default );
+        endforeach;
+
+        return $results;
+    }
 
     /**
-     * Persists a set of key => value pairs in the cache, with an optional TTL.
+     * Sets cache for key/value array.
      *
-     * @param iterable               $values A list of key => value pairs for a multiple-set operation.
-     * @param null|int|\DateInterval $ttl    Optional. The TTL value of this item. If no value is sent and
-     *                                       the driver supports TTL then the library may set a default value
-     *                                       for it or let the driver take care of that.
-     *
-     * @return bool True on success and false on failure.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $values is neither an array nor a Traversable,
-     *   or if any of the $values are not a legal value.
+     * @param Iterable $values to fetch.
+     * @param mixed $ttl expiration time.
      */
-    abstract public function setMultiple($values, $ttl = null);
+    public function setMultiple($values, $ttl = null)
+    {
+        $all_success = true;
+
+        foreach($values as $key => $value) :
+            $result = $this->set( $key, $value, $ttl );
+
+            if( $result === false ) :
+                $all_success = false;
+            endif;
+        endforeach;
+
+        return $all_success;
+    }
 
     /**
-     * Deletes multiple cache items in a single operation.
+     * Delete multiple transients.
      *
-     * @param iterable $keys A list of string-based keys to be deleted.
-     *
-     * @return bool True if the items were successfully removed. False if there was an error.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
+     * @param Iterable $keys toi delete.
+     * @return bool $transient true/false.
      */
-    abstract public function deleteMultiple($keys);
+    public function deleteMultiple($keys)
+    {
+        $all_deleted = true;
+
+        foreach($keys as $key) :
+            $result = $this->delete($key);
+
+            if( $result === false ) :
+                $all_deleted = false;
+            endif;
+        endforeach;
+
+        return $all_deleted;
+    }
 
     /**
      * Gets namespaced key.
